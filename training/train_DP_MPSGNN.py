69d07adbca63e1779dab7e0a8878e3780ae61feb
Andrea-1704 <s327644@studenti.polito.it>
2025-07-02 12:27:04 +0000

    Minor change

--- training/train_DP_MPSGNN.py
+++ training/train_DP_MPSGNN.py
@@ -123,8 +123,8 @@ def train2():
     train_mask_full = data_full['drivers'].train_mask
     y_bin_full = binarize_targets(y_full, threshold=10)
 
-    hidden_channels = 512
-    out_channels = 256
+    hidden_channels = 256
+    out_channels = 128
 
     metapaths, metapath_counts = beam_metapath_search_with_bags_learned(
         col_stats_dict = col_stats_dict_full,
@@ -132,7 +132,7 @@ def train2():
         y=y_full,
         train_mask=train_mask_full,
         node_type='drivers',
-        L_max=2, #----> tune
+        L_max=1, #----> tune
         channels = hidden_channels, 
         beam_width = 4
     )  
