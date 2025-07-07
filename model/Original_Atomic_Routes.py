349bf7caef0c92940daa3400bd236d01989b8621
Andrea-1704 <s327644@studenti.polito.it>
2025-07-07 15:14:06 +0200

    Minor stuff

--- model/Original_Atomic_Routes.py
+++ model/Original_Atomic_Routes.py
@@ -1,11 +1,3 @@
-
-#####
-# Implementing a Composite Message Passing with Atomic routes
-# This code is based on the paper https://arxiv.org/abs/2502.06784.
-####
-
-
-
 import numpy as np
 import math
 from tqdm import tqdm
@@ -99,6 +91,7 @@ import sys
 import os
 sys.path.append(os.path.abspath("."))
 
+from torch.optim.lr_scheduler import CosineAnnealingLR
 from model.MPSGNN_Model import MPSGNN
 from data_management.data import loader_dict_fn, merge_text_columns_to_categorical
 from utils.mpsgnn_metapath_utils import binarize_targets # binarize_targets sarà usata qui
@@ -331,107 +324,8 @@ class RelGNNConv(TransformerConv):
         out = super().forward((src_attn, dst_attn), edge_attn, edge_attr, return_attention_weights)
 
         return self.final_proj(out), src_attn
+    
 
-dataset = get_dataset("rel-f1", download=True)
-task = get_task("rel-f1", "driver-top3", download=True)
-
-train_table = task.get_table("train")
-val_table = task.get_table("val")
-test_table = task.get_table("test")
-
-out_channels = 1
-
-#loss_fn = nn.BCEWithLogitsLoss()
-tune_metric = "f1"
-higher_is_better = True #is referred to the tune metric
-
-seed_everything(42) #We should remember to try results 5 times with
-#different seed values to provide a confidence interval over results.
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-root_dir = "./data"
-
-db = dataset.get_db()
-col_to_stype_dict = get_stype_proposal(db)
-db_nuovo, col_to_stype_dict_nuovo = merge_text_columns_to_categorical(db, col_to_stype_dict)
-
-data_official, col_stats_dict_official = make_pkey_fkey_graph(
-    db_nuovo,
-    col_to_stype_dict=col_to_stype_dict_nuovo,
-    text_embedder_cfg = None,
-    cache_dir=None
-)
-#do not use the textual information: this db is mostly not textual
-
-graph_driver_ids = db_nuovo.table_dict["drivers"].df["driverId"].to_numpy()
-id_to_idx = {driver_id: idx for idx, driver_id in enumerate(graph_driver_ids)}
-
-
-train_df_raw = train_table.df
-driver_ids_raw = train_df_raw["driverId"].to_numpy()
-qualifying_positions = train_df_raw["qualifying"].to_numpy() #labels (train)
-
-
-binary_top3_labels_raw = qualifying_positions #do not need to binarize 
-#since the task is already a binary classification task
-
-
-target_vector_official = torch.full((len(graph_driver_ids),), float("nan"))
-for i, driver_id in enumerate(driver_ids_raw):
-    if driver_id in id_to_idx:#if the driver is in the training
-        target_vector_official[id_to_idx[driver_id]] = binary_top3_labels_raw[i]
-
-
-data_official['drivers'].y = target_vector_official.float()
-data_official['drivers'].train_mask = ~torch.isnan(target_vector_official)
-y_full = data_official['drivers'].y.float()
-train_mask_full = data_official['drivers'].train_mask
-num_pos = (y_full[train_mask_full] == 1).sum()
-num_neg = (y_full[train_mask_full] == 0).sum()
-pos_weight = torch.tensor([num_neg / num_pos], device=device)
-
-
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-loss_fn = nn.BCEWithLogitsLoss(pos_weight=pos_weight)
-
-hidden_channels = 128
-out_channels = 128
-
-loader_dict = loader_dict_fn(
-    batch_size=1024,
-    num_neighbours=512,
-    data=data_official,
-    task=task,
-    train_table=train_table,
-    val_table=val_table,
-    test_table=test_table
-)
-lr=1e-02
-wd=0
-
-
-col_stats_dict = col_stats_dict_official
-data=data_official
-db= db_nuovo
-node_id='driverId'
-train_mask=train_mask_full
-node_type='drivers'
-L_max=4
-channels = hidden_channels
-number_of_metapaths = 3  
-out_channels = out_channels
-hidden_channels = hidden_channels
-loader_dict = loader_dict
-lr = lr
-wd = wd
-task = task
-loss_fn= loss_fn
-epochs = 100
-tune_metric = tune_metric
-higher_is_better= higher_is_better
-
-
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 class RelGNN(torch.nn.Module):
     def __init__(
@@ -616,7 +510,7 @@ class RelGNN_Model(torch.nn.Module):
         )
 
         return self.head(x_dict[dst_table])
-
+    
 
 def get_atomic_routes(edge_type_list):
     """
@@ -648,94 +542,3 @@ def get_atomic_routes(edge_type_list):
                         atomic_routes_list.append(('dim-fact-dim',) + edge_q + get_rev_edge(edge_v))
 
     return atomic_routes_list
-#print(data.edge_index_dict)
-res = get_atomic_routes(data.edge_index_dict)
-
-model = RelGNN_Model(
-    data=data,
-    col_stats_dict=col_stats_dict,
-    out_channels=out_channels,
-    norm="batch_norm",
-    atomic_routes=res,
-    aggr="max",
-    channels=128,
-    num_model_layers=3,
-).to(device)
-
-
-
-
-optimizer = torch.optim.Adam(
-    model.parameters(),
-    lr=0.0005,
-    weight_decay=0
-)
-
-scheduler = CosineAnnealingLR(optimizer, T_max=100)
-
-
-early_stopping = EarlyStopping(
-    patience=30,
-    delta=0.0,
-    verbose=True,
-    path="best_basic_model.pt"
-)
-
-loader_dict = loader_dict_fn(
-    batch_size=1024, 
-    num_neighbours=512, 
-    data=data, 
-    task=task,
-    train_table=train_table, 
-    val_table=val_table, 
-    test_table=test_table
-)
-
-
-
-
-# Training loop
-epochs = 100
-
-state_dict = None
-test_table = task.get_table("test", mask_input_cols=False)
-best_val_metric = -math.inf if higher_is_better else math.inf
-best_test_metric = -math.inf if higher_is_better else math.inf
-for epoch in range(1, epochs + 1):
-    train_loss = train(model, optimizer, loader_dict=loader_dict, device=device, task=task, loss_fn=loss_fn)
-
-    train_pred = test(model, loader_dict["train"], device=device, task=task)
-    train_metrics = evaluate_performance(train_pred, train_table, task.metrics, task=task)
-    train_mae_preciso = evaluate_on_full_train(model, loader_dict["train"], device=device, task=task)
-
-    val_pred = test(model, loader_dict["val"], device=device, task=task)
-    val_metrics = evaluate_performance(val_pred, val_table, task.metrics, task=task)
-
-    test_pred = test(model, loader_dict["test"], device=device, task=task)
-    test_metrics = evaluate_performance(test_pred, test_table, task.metrics, task=task)
-
-    scheduler.step(val_metrics[tune_metric])
-
-    if (higher_is_better and val_metrics[tune_metric] > best_val_metric) or (
-            not higher_is_better and val_metrics[tune_metric] < best_val_metric
-    ):
-        best_val_metric = val_metrics[tune_metric]
-        state_dict = copy.deepcopy(model.state_dict())
-
-    #test:
-    if (higher_is_better and test_metrics[tune_metric] > best_test_metric) or (
-            not higher_is_better and test_metrics[tune_metric] < best_test_metric
-    ):
-        best_test_metric = test_metrics[tune_metric]
-        state_dict_test = copy.deepcopy(model.state_dict())
-
-    current_lr = optimizer.param_groups[0]["lr"]
-    print(f"Epoch: {epoch:02d}, Train {tune_metric}: {train_mae_preciso:.2f}, Validation {tune_metric}: {val_metrics[tune_metric]:.2f}, Test {tune_metric}: {test_metrics[tune_metric]:.2f}, LR: {current_lr:.6f}")
-
-    early_stopping(val_metrics[tune_metric], model)
-
-    if early_stopping.early_stop:
-        print(f"Early stopping triggered at epoch {epoch}")
-        break
-print(f"best validation results: {best_val_metric}")
-print(f"best test results: {best_test_metric}")
