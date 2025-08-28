baa62b8af2b509f4915f93b212c65b12c48d3844
Andrea-1704 <s327644@studenti.polito.it>
2025-08-28 19:08:59 +0200

    Trying to add same metapath statics for extension 1

--- utils/XMetapath_utils/XMetapath_extension_1.py
+++ utils/XMetapath_utils/XMetapath_extension_1.py
@@ -24,19 +24,28 @@ from relbench.modeling.nn import HeteroEncoder
 from collections import defaultdict
 from model.XMetaPath import XMetapath
 from utils.utils import evaluate_performance, test, train
-import numpy as np
 import collections 
 
+
+
 def flip_rel(rel_name: str) -> str:
+    """
+    Converts the name of the relation to its reversed version.
+    """
     return rel_name[4:] if rel_name.startswith("rev_") else f"rev_{rel_name}"
 
 def to_canonical(mp_outward):
     """
-    Converte un path costruito 'in avanti' (da drivers a ...) nella forma canonica
-    usata dal modello (... -> 'drivers'), con flip dei nomi relazionali.
+    This function takes as input the metapath found by the model and converts
+    it to a metapath with reversed paths, meaning that the source of each 
+    path becomes destination, the destination the source and the name of the 
+    relation gets converted (flip_rel).
+    
+    It should be verified that mp[-1][2] == target node. 
     """
     mp = [(dst, flip_rel(rel), src) for (src, rel, dst) in mp_outward[::-1]]
-    assert mp[-1][2] == "drivers"
+    # assert mp[-1][2] == "drivers"
+    print(f"changed metapath order from {mp_outward} to {mp}")
     return tuple(mp)
 
 
