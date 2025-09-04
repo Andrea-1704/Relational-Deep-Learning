02521b29fa430412c7936801d90cfb4518816863
Andrea-1704 <s327644@studenti.polito.it>
2025-09-04 21:00:40 +0200

    It must be tested 'train_XMetaPAth.py' in Driver position

--- training/rel_f1/relF1_DP/train_XMetaPath.py
+++ training/rel_f1/relF1_DP/train_XMetaPath.py
@@ -1,210 +1,194 @@
 import torch
-import torch.nn.functional as F
-
 import os
 import torch
-import relbench
-import numpy as np
-from torch.nn import BCEWithLogitsLoss, L1Loss
+from torch.nn import L1Loss
 from relbench.datasets import get_dataset
 from relbench.tasks import get_task
 import math
-from tqdm import tqdm
-import torch_geometric
-import torch_frame
 from torch_geometric.seed import seed_everything
 from relbench.modeling.utils import get_stype_proposal
-from collections import defaultdict
-import requests
-from io import StringIO
-from torch_frame.config.text_embedder import TextEmbedderConfig
 from relbench.modeling.graph import make_pkey_fkey_graph
-from torch.nn import BCEWithLogitsLoss
-import copy
-from typing import Any, Dict, List
-from torch import Tensor, batch_norm_gather_stats
-from torch.nn import Embedding, ModuleDict
-from torch.optim.lr_scheduler import CosineAnnealingLR
-from torch_frame.data.stats import StatType
-
 import sys
-import os
+import copy
 sys.path.append(os.path.abspath("."))
 
-from model.XMetaPath import XMetaPath
+from model.XMetaPath2 import XMetaPath2
 from data_management.data import loader_dict_fn, merge_text_columns_to_categorical
-from utils.utils import evaluate_performance, evaluate_on_full_train, test, train
-from utils.EarlyStopping import EarlyStopping
-from utils.XMetapath_utils.XMetaPath_metapath_utils import greedy_metapath_search_with_bags_learned, greedy_metapath_search_with_bags_learned_2, greedy_metapath_search_with_bags_learned_3, beam_metapath_search_with_bags_learned_2
-#from utils.mapping_utils import get_global_to_local_id_map
+from utils.utils import evaluate_performance, test, train
+from utils.XMetapath_utils.XMetaPath_extension4 import RLAgent, warmup_rl_agent, final_metapath_search_with_rl
 
 
-def train2():
-    dataset = get_dataset("rel-f1", download=True)
-    task = get_task("rel-f1", "driver-position", download=True)
-
-    train_table = task.get_table("train") #date  driverId  qualifying
-    val_table = task.get_table("val") #date  driverId  qualifying
-    test_table = task.get_table("test") # date  driverId
-
-    out_channels = 1
-    loss_fn = L1Loss()
-    tune_metric = "mae"
-    higher_is_better = False
-
-    seed_everything(42)
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    root_dir = "./data"
-
-    db = dataset.get_db() #get all tables
-    col_to_stype_dict = get_stype_proposal(db)
-    #this is used to get the stype of the columns
-
-    #let's use the merge categorical values:
-    db_nuovo, col_to_stype_dict_nuovo = merge_text_columns_to_categorical(db, col_to_stype_dict)
-
-    # Create the graph
-    data_official, col_stats_dict_official = make_pkey_fkey_graph(
-        db_nuovo,
-        col_to_stype_dict=col_to_stype_dict_nuovo,
-        text_embedder_cfg = None,
-        cache_dir=None  # disabled
-    )
-
-    graph_driver_ids = db_nuovo.table_dict["drivers"].df["driverId"].to_numpy()
-    id_to_idx = {driver_id: idx for idx, driver_id in enumerate(graph_driver_ids)}
-
-    train_df = train_table.df
-    driver_labels = train_df["position"].to_numpy()
-    driver_ids = train_df["driverId"].to_numpy()
-
-    target_vector = torch.full((len(graph_driver_ids),), float("nan"))
-    for i, driver_id in enumerate(driver_ids):
-        if driver_id in id_to_idx:
-            target_vector[id_to_idx[driver_id]] = driver_labels[i]
-
-    data_official['drivers'].y = target_vector
-    data_official['drivers'].train_mask = ~torch.isnan(target_vector)
-
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-    data_full, col_stats_dict_full = make_pkey_fkey_graph(
-        db_nuovo,
-        col_to_stype_dict=col_to_stype_dict_nuovo,
-        text_embedder_cfg=None,
-        cache_dir=None
-    )
-    data_full = data_full.to(device)
-
-    #retrieve the id from the driver nodes
-    graph_driver_ids = db_nuovo.table_dict["drivers"].df["driverId"].to_numpy()
-    id_to_idx = {driver_id: idx for idx, driver_id in enumerate(graph_driver_ids)}
-
-    #get the labels and the ids of the drivers from the table
-    train_df = train_table.df
-    driver_labels = train_df["position"].to_numpy()
-    driver_ids = train_df["driverId"].to_numpy()
-
-    #map the correct labels for all drivers node (which are target ones)
-    target_vector = torch.full((len(graph_driver_ids),), float("nan")) #inizial
-    for i, driver_id in enumerate(driver_ids):
-        if driver_id in id_to_idx:
-            target_vector[id_to_idx[driver_id]] = driver_labels[i]
-
-    
-    data_full['drivers'].y = target_vector
-    data_full['drivers'].train_mask = ~torch.isnan(target_vector)
-
-    #take y and mask complete for the dataset:
-    y_full = data_full['drivers'].y.float()
-    train_mask_full = data_full['drivers'].train_mask
-    y_bin_full = y_full
-    
-    hidden_channels = 128
-    out_channels = 128
-
-    loader_dict = loader_dict_fn(
-        batch_size=1024,
-        num_neighbours=512,
-        data=data_official,
-        task=task,
-        train_table=train_table,
-        val_table=val_table,
-        test_table=test_table
-    )
-    lr=0.0001
-    wd = 0
-
-    metapaths, metapath_counts = greedy_metapath_search_with_bags_learned_3(
-        col_stats_dict = col_stats_dict_official,
-        data=data_official,
-        db= db_nuovo,
-        node_id='driverId',
-        train_mask=train_mask_full,
-        node_type='drivers',
-        L_max=3,
-        channels = hidden_channels,
-        number_of_metapaths = 4,     
-        out_channels = out_channels,
-        hidden_channels = hidden_channels, 
-        loader_dict = loader_dict,
-        lr = lr,
-        wd = wd,
-        task = task,
-        loss_fn= loss_fn, 
-        epochs = 100, 
-        tune_metric = tune_metric,
-        higher_is_better= higher_is_better
-    )
-
-    print(f"\nfinal metapaths are {metapaths}\n")
-    print(f"\nmetapaths counts are {metapath_counts}\n")
-
-    model = MPSGNN(
-        data=data_official,
-        col_stats_dict=col_stats_dict_official,
-        metadata=data_full.metadata(),
-        metapath_counts = metapath_counts,
-        metapaths=metapaths,
-        hidden_channels=hidden_channels, #----> tune
-        out_channels=out_channels,    #----> tune
-        final_out_channels=1, 
-    ).to(device)
-
-    lr=1e-02
-    wd=0
-
-    optimizer = torch.optim.SGD(model.parameters(), lr=lr, momentum=0.9, weight_decay=wd)
-    
-
-    scheduler = CosineAnnealingLR(optimizer, T_max=25)  #---> tune
+# utility functions:
+#############################################
+def flip_rel(rel_name: str) -> str:
+    return rel_name[4:] if rel_name.startswith("rev_") else f"rev_{rel_name}"
+def to_canonical(mp_outward):
+    mp = [(dst, flip_rel(rel), src) for (src, rel, dst) in mp_outward[::-1]]
+    #The assert is done in the caller (see code below)
+    return tuple(mp)
+#############################################
 
 
-    early_stopping = EarlyStopping(
-        patience=60,
-        delta=0.0,
-        verbose=True,
-        path="best_basic_model.pt"
-    )
+#Configuration for the task:
+#############################################
+task_name = "driver-position"
+db_name = "rel_f1"
+node_id = "driverId"
+target = "position"
+node_type = "drivers"
+dataset = get_dataset(db_name, download=True)
+task = get_task(db_name, task_name, download=True)
+task_type = task.task_type
+out_channels = 1
+tune_metric = "mae"
+higher_is_better = False
+loss_fn = L1Loss()
+#############################################
 
-    test_table = task.get_table("test", mask_input_cols=False)
-    best_test_metrics = -math.inf if higher_is_better else math.inf
-    epochs = 200
-    for _ in range(0, epochs):
-        train(model, optimizer, loader_dict=loader_dict, device=device, task=task, loss_fn=loss_fn)
-        test_pred = test(model, loader_dict["test"], device=device, task=task)
-        test_metrics = evaluate_performance(test_pred, test_table, task.metrics, task=task)
-        if test_metrics[tune_metric] > best_test_metrics and higher_is_better:
-            best_test_metrics = test_metrics[tune_metric]
-        if test_metrics[tune_metric] < best_test_metrics and not higher_is_better:
-            best_test_metrics = test_metrics[tune_metric]
-    print(f"We obtain F1 test loss equal to {best_test_metrics}")
 
-      
+train_table = task.get_table("train")
+val_table = task.get_table("val")
+test_table = task.get_table("test")
+seed = 42
+seed_everything(seed) 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+root_dir = "./data"
 
 
 
+#Configuration for a node regression task:
+#############################################
+db = dataset.get_db() #get all tables
+col_to_stype_dict = get_stype_proposal(db)
+db_nuovo, col_to_stype_dict_nuovo = merge_text_columns_to_categorical(db, col_to_stype_dict)
+data_official, col_stats_dict_official = make_pkey_fkey_graph(
+    db_nuovo,
+    col_to_stype_dict=col_to_stype_dict_nuovo,
+    text_embedder_cfg = None,
+    cache_dir=None  # disabled
+)
+graph_driver_ids = db_nuovo.table_dict[node_type].df[node_id].to_numpy()
+id_to_idx = {driver_id: idx for idx, driver_id in enumerate(graph_driver_ids)}
+train_df = train_table.df
+driver_labels = train_df[target].to_numpy()
+driver_ids = train_df[node_id].to_numpy()
+target_vector = torch.full((len(graph_driver_ids),), float("nan"))
+for i, driver_id in enumerate(driver_ids):
+    if driver_id in id_to_idx:
+        target_vector[id_to_idx[driver_id]] = driver_labels[i]
+data_official[node_type].y = target_vector
+data_official[node_type].train_mask = ~torch.isnan(target_vector)
+y_full = data_official[node_type].y.float()
+train_mask_full = data_official[node_type].train_mask
+y_bin_full = y_full
+#############################################
 
 
-if __name__ == '__main__':
-    train2()
+loss_fn = L1Loss()
+hidden_channels = 128
+out_channels = 128
+loader_dict = loader_dict_fn(
+    batch_size=512,
+    num_neighbours=256,
+    data=data_official,
+    task=task,
+    train_table=train_table,
+    val_table=val_table,
+    test_table=test_table
+)
+
+
+
+#Learn the most useful metapaths:
+#or:
+#metapaths = [[('drivers', 'rev_f2p_driverId', 'standings'), ('standings', 'f2p_raceId', 'races')], [('drivers', 'rev_f2p_driverId', 'qualifying'), ('qualifying', 'f2p_constructorId', 'constructors'), ('constructors', 'rev_f2p_constructorId', 'constructor_results'), ('constructor_results', 'f2p_raceId', 'races')], [('drivers', 'rev_f2p_driverId', 'results'), ('results', 'f2p_constructorId', 'constructors'), ('constructors', 'rev_f2p_constructorId', 'constructor_standings'), ('constructor_standings', 'f2p_raceId', 'races')]]
+#############################################
+agent = RLAgent(tau=1.0, alpha=0.5)
+agent.best_score_by_path_global.clear() 
+warmup_rl_agent(
+    agent=agent,
+    data=data_official,
+    loader_dict=loader_dict,
+    task=task,
+    loss_fn=loss_fn,
+    tune_metric=tune_metric,
+    higher_is_better=higher_is_better,
+    train_mask=train_mask_full,
+    node_type=node_type,
+    col_stats_dict=col_stats_dict_official,
+    num_episodes=3,   
+    L_max=4,          
+    epochs=3         
+)
+K = 3
+global_best_map = agent.best_score_by_path_global
+agent.tau = 0.3   
+agent.alpha = 0.2 
+metapaths, metapath_count = final_metapath_search_with_rl(
+    agent=agent,
+    data=data_official,
+    loader_dict=loader_dict,
+    task=task,
+    loss_fn=loss_fn,
+    tune_metric=tune_metric,
+    higher_is_better=higher_is_better,
+    train_mask=train_mask_full,
+    node_type=node_type,
+    col_stats_dict=col_stats_dict_official,
+    L_max=4,                 
+    epochs=10,
+    number_of_metapaths=K    
+)
+canonical = []
+for mp in metapaths:
+    #change to canonical:
+    mp = mp.copy()
+    mp_key   = to_canonical(mp)         
+    assert mp_key[-1][2] == node_type, \
+        f"Il meta-path canonico deve terminare su '{node_type}', invece termina su '{mp_key[-1][2]}'"
+    canonical.append(mp_key)
+print(f"Canonical metapaths are: {canonical}")
+#############################################
+
+
+#Train the final model with the metapaths found:
+#############################################
+model = XMetaPath2(
+    data=data_official,
+    col_stats_dict=col_stats_dict_official,
+    metapaths=canonical,               
+    hidden_channels=hidden_channels,
+    out_channels=out_channels,
+    final_out_channels=1,
+).to(device)
+lr=0.0005
+wd = 0
+optimizer = torch.optim.AdamW(
+    model.parameters(),
+    lr=lr,
+    weight_decay=wd
+)
+best_val_metric = -math.inf 
+test_table = task.get_table("test", mask_input_cols=False)
+best_test_metric = -math.inf 
+epochs = 150
+for epoch in range(0, epochs):
+    train_loss = train(model, optimizer, loader_dict=loader_dict, device=device, task=task, loss_fn=loss_fn)
+    train_pred = test(model, loader_dict["train"], device=device, task=task)
+    val_pred = test(model, loader_dict["val"], device=device, task=task)
+    test_pred = test(model, loader_dict["test"], device=device, task=task)
+    train_metrics = evaluate_performance(train_pred, train_table, task.metrics, task=task)
+    val_metrics = evaluate_performance(val_pred, val_table, task.metrics, task=task)
+    test_metrics = evaluate_performance(test_pred, test_table, task.metrics, task=task)
+    if (higher_is_better and val_metrics[tune_metric] > best_val_metric):
+        best_val_metric = val_metrics[tune_metric]
+        state_dict = copy.deepcopy(model.state_dict())
+    if (higher_is_better and test_metrics[tune_metric] > best_test_metric):
+        best_test_metric = test_metrics[tune_metric]
+        state_dict_test = copy.deepcopy(model.state_dict())
+    print(f"Epoch: {epoch:02d}, Train {tune_metric}: {train_metrics[tune_metric]:.2f}, Validation {tune_metric}: {val_metrics[tune_metric]:.2f}, Test {tune_metric}: {test_metrics[tune_metric]:.2f}")
+#############################################
+
+#Final results:
+print(f"best validation results: {best_val_metric}")
+print(f"best test results: {best_test_metric}")
--- training/rel_f1/relF1_driverTop3/benchmark_execution_XMetapath.py
+++ training/rel_f1/relF1_driverTop3/benchmark_execution_XMetapath.py
@@ -120,6 +120,8 @@ loader_dict = loader_dict_fn(
 
 
 #Learn the most useful metapaths:
+#or:
+#metapaths = [[('drivers', 'rev_f2p_driverId', 'standings'), ('standings', 'f2p_raceId', 'races')], [('drivers', 'rev_f2p_driverId', 'qualifying'), ('qualifying', 'f2p_constructorId', 'constructors'), ('constructors', 'rev_f2p_constructorId', 'constructor_results'), ('constructor_results', 'f2p_raceId', 'races')], [('drivers', 'rev_f2p_driverId', 'results'), ('results', 'f2p_constructorId', 'constructors'), ('constructors', 'rev_f2p_constructorId', 'constructor_standings'), ('constructor_standings', 'f2p_raceId', 'races')]]
 #############################################
 agent = RLAgent(tau=1.0, alpha=0.5)
 agent.best_score_by_path_global.clear() 
