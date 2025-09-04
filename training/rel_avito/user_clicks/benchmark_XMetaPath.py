a3d1fb47b54af9a57143eab599f64b87d436551d
Andrea-1704 <s327644@studenti.polito.it>
2025-09-04 20:35:45 +0200

    Solving problem with user_clicks task: the label are boolean and we should therefore convert them in binary values

--- training/rel_avito/user_clicks/benchmark_XMetaPath.py
+++ training/rel_avito/user_clicks/benchmark_XMetaPath.py
@@ -83,11 +83,18 @@ train_df_raw = train_table.df
 driver_ids_raw = train_df_raw[node_id].to_numpy()
 print(f"this is train_df_raw  {train_df_raw}")
 binary_top3_labels_raw = train_df_raw[target].to_numpy() 
-target_vector_official = torch.full((len(graph_driver_ids),), float("nan")) #inizialize a vector with all "nan" elements
+#ONLY IN THIS CASE IN WHICH THE LABEL IS FALSE/TRUE INSTEAD OF 0/1->CONVERSION:
+target_vector_official = torch.full(
+    (len(graph_driver_ids),),
+    float("nan"),
+    dtype=torch.float32
+)
+labels_np = binary_top3_labels_raw.astype("float32")
 for i, driver_id in enumerate(driver_ids_raw):
-    if driver_id in id_to_idx:#if the driver is in the training
-        target_vector_official[id_to_idx[driver_id]] = binary_top3_labels_raw[i]
-data_official[node_type].y = target_vector_official.float()
+    j = id_to_idx.get(driver_id)
+    if j is not None:
+        target_vector_official[j] = float(labels_np[i])
+data_official[node_type].y = target_vector_official
 data_official[node_type].train_mask = ~torch.isnan(target_vector_official)
 y_full = data_official[node_type].y.float()
 train_mask_full = data_official[node_type].train_mask
