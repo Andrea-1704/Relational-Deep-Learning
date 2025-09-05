e636e2656e147b161a181afeb6da176024b10653
Andrea-1704 <s327644@studenti.polito.it>
2025-09-05 23:35:40 +0200

    Trying something realy new

--- training/rel_f1/relF1_driverTop3/new_benchmark.py
+++ training/rel_f1/relF1_driverTop3/new_benchmark.py
@@ -123,44 +123,44 @@ loader_dict = loader_dict_fn(
 
 #Learn the most useful metapaths:
 #or:
-#metapaths = [[('drivers', 'rev_f2p_driverId', 'standings'), ('standings', 'f2p_raceId', 'races')], [('drivers', 'rev_f2p_driverId', 'qualifying'), ('qualifying', 'f2p_constructorId', 'constructors'), ('constructors', 'rev_f2p_constructorId', 'constructor_results'), ('constructor_results', 'f2p_raceId', 'races')], [('drivers', 'rev_f2p_driverId', 'results'), ('results', 'f2p_constructorId', 'constructors'), ('constructors', 'rev_f2p_constructorId', 'constructor_standings'), ('constructor_standings', 'f2p_raceId', 'races')]]
+metapaths = [[('drivers', 'rev_f2p_driverId', 'standings'), ('standings', 'f2p_raceId', 'races')], [('drivers', 'rev_f2p_driverId', 'qualifying'), ('qualifying', 'f2p_constructorId', 'constructors'), ('constructors', 'rev_f2p_constructorId', 'constructor_results'), ('constructor_results', 'f2p_raceId', 'races')], [('drivers', 'rev_f2p_driverId', 'results'), ('results', 'f2p_constructorId', 'constructors'), ('constructors', 'rev_f2p_constructorId', 'constructor_standings'), ('constructor_standings', 'f2p_raceId', 'races')]]
 #############################################
-agent = RLAgent(tau=1.0, alpha=0.5)
-agent.best_score_by_path_global.clear() 
-warmup_rl_agent(
-    agent=agent,
-    data=data_official,
-    loader_dict=loader_dict,
-    task=task,
-    loss_fn=loss_fn,
-    tune_metric=tune_metric,
-    higher_is_better=higher_is_better,
-    train_mask=train_mask_full,
-    node_type=node_type,
-    col_stats_dict=col_stats_dict_official,
-    num_episodes=3,   
-    L_max=4,          
-    epochs=3         
-)
-K = 3
-global_best_map = agent.best_score_by_path_global
-agent.tau = 0.3   
-agent.alpha = 0.2 
-metapaths, metapath_count = final_metapath_search_with_rl(
-    agent=agent,
-    data=data_official,
-    loader_dict=loader_dict,
-    task=task,
-    loss_fn=loss_fn,
-    tune_metric=tune_metric,
-    higher_is_better=higher_is_better,
-    train_mask=train_mask_full,
-    node_type=node_type,
-    col_stats_dict=col_stats_dict_official,
-    L_max=4,                 
-    epochs=10,
-    number_of_metapaths=K    
-)
+# agent = RLAgent(tau=1.0, alpha=0.5)
+# agent.best_score_by_path_global.clear() 
+# warmup_rl_agent(
+#     agent=agent,
+#     data=data_official,
+#     loader_dict=loader_dict,
+#     task=task,
+#     loss_fn=loss_fn,
+#     tune_metric=tune_metric,
+#     higher_is_better=higher_is_better,
+#     train_mask=train_mask_full,
+#     node_type=node_type,
+#     col_stats_dict=col_stats_dict_official,
+#     num_episodes=3,   
+#     L_max=4,          
+#     epochs=3         
+# )
+# K = 3
+# global_best_map = agent.best_score_by_path_global
+# agent.tau = 0.3   
+# agent.alpha = 0.2 
+# metapaths, metapath_count = final_metapath_search_with_rl(
+#     agent=agent,
+#     data=data_official,
+#     loader_dict=loader_dict,
+#     task=task,
+#     loss_fn=loss_fn,
+#     tune_metric=tune_metric,
+#     higher_is_better=higher_is_better,
+#     train_mask=train_mask_full,
+#     node_type=node_type,
+#     col_stats_dict=col_stats_dict_official,
+#     L_max=4,                 
+#     epochs=10,
+#     number_of_metapaths=K    
+# )
 canonical = []
 for mp in metapaths:
     #change to canonical:
