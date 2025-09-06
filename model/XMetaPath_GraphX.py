d1b8a73107afcf935b9896e306bac0831d827f30
Andrea-1704 <s327644@studenti.polito.it>
2025-09-06 10:24:09 +0200

    Change hyper params new approach

--- model/XMetaPath_GraphX.py
+++ model/XMetaPath_GraphX.py
@@ -244,7 +244,7 @@ class MetaPathGNN(nn.Module):
             #self.convs.append(MetaPathGNNLayer(hidden_channels, hidden_channels, relation_index=i))
             #self.convs.append(MetaPathGNNLayer(hidden_channels))
             self.convs.append(
-                GraphX(d_model=hidden_channels, num_heads=8,
+                GraphX(d_model=hidden_channels, num_heads=32,
                     time_bias='linear', time_scale=1.0,
                     time_max_bucket=256, time_bucket_base=1.6,
                     dropout=0.1)
