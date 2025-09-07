6bc1725eb9f826c51b06413c03fd1ff54bdb4cb2
Andrea-1704 <s327644@studenti.polito.it>
2025-09-07 19:15:50 +0200

    Change param site

--- training/rel_trial/site_success/benchmark_XMetaPath.py
+++ training/rel_trial/site_success/benchmark_XMetaPath.py
@@ -128,9 +128,9 @@ warmup_rl_agent(
     train_mask=train_mask_full,
     node_type=node_type,
     col_stats_dict=col_stats_dict_official,
-    num_episodes=3,   
-    L_max=4,          
-    epochs=3        
+    num_episodes=10,   
+    L_max=6,          
+    epochs=10       
 )
 K = 3
 global_best_map = agent.best_score_by_path_global
@@ -147,8 +147,8 @@ metapaths, metapath_count = final_metapath_search_with_rl(
     train_mask=train_mask_full,
     node_type=node_type,
     col_stats_dict=col_stats_dict_official,
-    L_max=4,                 
-    epochs=10,
+    L_max=6,                 
+    epochs=20,
     number_of_metapaths=K    
 )
 canonical = []
