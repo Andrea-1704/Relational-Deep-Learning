6ca0195c939e91f4dd01a7561d3218a77ca1022b
Andrea-1704 <s327644@studenti.polito.it>
2025-09-19 11:58:15 +0200

    deleting scheduler graph

--- training/rel_f1/relF1_DP/train_DP_GraphormerNew.py
+++ training/rel_f1/relF1_DP/train_DP_GraphormerNew.py
@@ -113,7 +113,7 @@ optimizer = torch.optim.Adam(
     weight_decay=0.000001
 )
 
-scheduler = CosineAnnealingLR(optimizer, T_max=100)
+#scheduler = CosineAnnealingLR(optimizer, T_max=100)
 
 
 early_stopping = EarlyStopping(
@@ -156,7 +156,7 @@ for epoch in range(1, epochs + 1):
     test_pred = test(model, loader_dict["test"], device=device, task=task)
     test_metrics = evaluate_performance(test_pred, test_table, task.metrics, task=task)
 
-    scheduler.step(val_metrics[tune_metric])
+    #scheduler.step(val_metrics[tune_metric])
 
     if (higher_is_better and val_metrics[tune_metric] > best_val_metric) or (
             not higher_is_better and val_metrics[tune_metric] < best_val_metric
