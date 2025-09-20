a3a7b5d33cd76a59093dfeb61ffab1907329353e
Andrea-1704 <s327644@studenti.polito.it>
2025-09-20 19:07:07 +0200

    latrest pretraining tests

--- training/rel_trial/site_success/train_HGAT.py
+++ training/rel_trial/site_success/train_HGAT.py
@@ -101,13 +101,13 @@ model = Model(
     num_layers=4,
     channels=channels,
     out_channels=1,
-    aggr="sum",
+    aggr="max",
     norm="batch_norm",
 ).to(device)
 
 
 
-optimizer = torch.optim.AdamW(model.parameters(), lr=0.00001, weight_decay=0.0001)
+optimizer = torch.optim.AdamW(model.parameters(), lr=0.001, weight_decay=0.0001)
 
 
 
